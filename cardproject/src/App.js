b16bca65c7a5ec274abfba450d68d84ba7893ebf
canberk164 <canberk164@gmail.com>
2024-01-26 19:40:54 +0300

    updated

--- cardproject/src/App.js
+++ cardproject/src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
-import { Course } from "./Course";
+import Course from "./Course";
 
 function App() {
   return (
     <div className="App">
-      <Course />
-      <Course />
-      <Course />
-      <Course />
+      <Course
+        title="Angular"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
+      <Course
+        title="HTML"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
+      <Course
+        title="React"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
+      <Course
+        title="Frontend"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
     </div>
   );
 }
--- cardproject/src/Course.js
+++ cardproject/src/Course.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const Course = () => {
-  return <div>Course</div>;
-};
+function Course({ title, description }) {
+  // console.log(props);
+  // const title = props.title;
+  // const description = props.description;
+  // const { title, description } = props;
+
+  return (
+    <>
+      <div>{title}</div>
+      <div>{description}</div>
+    </>
+  );
+}
+
+export default Course;
